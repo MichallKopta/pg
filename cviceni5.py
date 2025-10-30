93af9c23228780a7d4de6d1f1eecc9ff48ed6159
MichallKopta <michallkopta@gmail.com>
2025-10-30 09:55:25 +0000

    cviceni

--- cviceni5.py
+++ cviceni5.py
@@ -20,6 +20,27 @@ def zapis_data_do_csv(file_name, data):
             #row2 = [str(value) for value in row]
             line = ','.join([str(value) for value in row])
             file.write(line + "\n")
+
+def precti_hodnoty_a_incrementuj2(file):
+    data = []
+    reader = csv.reader(file)
+    for row in reader:
+        line = []
+        for value in row:
+            try:
+                value = int(value)
+            except TypeError:
+                pass
+            line.append(value)
+        data.append(row)
+    return data
+
+def zapis_data_do_csv2(file_name, data):
+    with open(file_name, "w") as file:
+        writer = csv.writer(file)
+        for row in data:
+            writer.writerows(data)
+
 if __name__ == "__main__":
     try:
         name = input("Zadej jmeno souboru: ")
